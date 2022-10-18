0d89e763f17c247bb6bcc78c75c22c2e1fb8cf75
chehin <chehinjuan@gmail.com>
2022-10-18 19:40:21 -0300

    logout

--- sistegeo-apk/src/app/app.component.ts
+++ sistegeo-apk/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './services/auth-service.service';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,7 @@ export class AppComponent {
   public appPages = [
     { title: 'Principal', url: '/home', icon: 'home' },
     { title: 'Acerca de', url: '/acercade', icon: 'information-circle' },
+    // { title: 'Log Out', url: '/logout', icon: 'log-out' },
     // { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
     // { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
     // { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
@@ -17,7 +19,9 @@ export class AppComponent {
   // public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   public labels = [];
 
-  constructor() {
+  constructor(
+    public authService: AuthService,
+    ) {
 
   }
 
