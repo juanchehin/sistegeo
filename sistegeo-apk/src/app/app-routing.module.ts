2cc4787b790ede6c4e04a695b2f37562274d3c01
chehin <chehinjuan@gmail.com>
2022-10-18 10:51:29 -0300

    guard

--- sistegeo-apk/src/app/app-routing.module.ts
+++ sistegeo-apk/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuardService } from './services/auth-guard.service';
 
 const routes: Routes = [
   {
@@ -17,7 +18,8 @@ const routes: Routes = [
   },
   {
     path: 'home',
-    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule),
+    canActivate: [AuthGuardService]
   },
   {
     path: 'login',
---
+++ sistegeo-apk/src/app/services/auth-guard.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, CanLoad, Router } from '@angular/router';
+import { AuthService } from './auth-service.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuardService implements CanActivate {
+
+  constructor(
+    public authService: AuthService, public router: Router
+  ) { }
+
+  canActivate() {
+    console.log("pasa estalogueado : ",this.authService.estaLogueado())
+    if ( this.authService.estaLogueado()) {
+      return true;
+     } else {
+       this.router.navigate(['/login']);
+       return false;
+    }
+  }
+
+}
